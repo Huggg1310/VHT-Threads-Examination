d82976df00325205d637f1454c509191709afaf9
Hoàng Tuấn Hưng <132705630+Huggg1310@users.noreply.github.com>
2026-03-17 18:12:03 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,16 +5,23 @@
 #include <time.h>
 #include <errno.h>
 #include <stdint.h>
+#include <signal.h>
 
 #define handle_error_en(en, msg) \
         do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 uint64_t T = 0;
-uint64_t X = 1000000; /* Chu kỳ mặc định 1000000 ns */
+uint64_t X = 1000000;
 int t_updated = 0;
+volatile int running = 1; 
 
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+void handle_sig(int sig) {
+    running = 0;
+    pthread_cond_broadcast(&cond);
+}
 
 uint64_t current_time_ns() {
     struct timespec ts;
@@ -28,8 +35,7 @@ uint64_t current_time_ns() {
 /* Thread 1: SAMPLE */
 static void * thread_sample(void *arg) {
     struct timespec req;
-    
-    while (1) { 
+    while (running) {
         uint64_t now = current_time_ns();
         
         pthread_mutex_lock(&mtx);
@@ -42,9 +48,7 @@ static void * thread_sample(void *arg) {
         req.tv_sec = current_x / 1000000000ULL;
         req.tv_nsec = current_x % 1000000000ULL;
         
-      
         if (nanosleep(&req, NULL) == -1 && errno != EINTR) {
-            perror("nanosleep");
         }
     }
     return NULL;
@@ -52,8 +56,8 @@ static void * thread_sample(void *arg) {
 
 /* Thread 2: INPUT */
 static void * thread_input(void *arg) {
-    while (1) {
-        FILE *fp = fopen("freq.txt", "r");
+    while (running) {
+        FILE *fp = fopen("logs/freq.txt", "r");
         if (fp != NULL) {
             uint64_t new_x;
             if (fscanf(fp, "%llu", &new_x) == 1) {
@@ -70,27 +74,32 @@ static void * thread_input(void *arg) {
 
 /* Thread 3: LOGGING */
 static void * thread_logging(void *arg) {
-    FILE *fp = fopen("time_and_interval.txt", "w");
+    FILE *fp = fopen("logs/time_and_interval.txt", "w");
     if (fp == NULL) {
-        perror("fopen time_and_interval.txt");
+        perror("fopen logs/time_and_interval.txt");
         return NULL;
     }
 
     uint64_t prev_T = 0;
 
-    while (1) {
+    while (running) {
         pthread_mutex_lock(&mtx);
-        while (t_updated == 0) {
+        while (t_updated == 0 && running) {
             pthread_cond_wait(&cond, &mtx);
         }
+        if (!running) {
+            pthread_mutex_unlock(&mtx);
+            break;
+        }
         uint64_t current_T = T;
+        uint64_t current_X = X; 
         t_updated = 0;
         pthread_mutex_unlock(&mtx);
 
         uint64_t interval = (prev_T == 0) ? 0 : (current_T - prev_T);
         prev_T = current_T;
 
-        fprintf(fp, "%llu %llu\n", current_T, interval);
+        fprintf(fp, "%llu %llu %llu\n", current_T, interval, current_X);
         fflush(fp); 
     }
 
@@ -102,7 +111,10 @@ int main(int argc, char *argv[]) {
     int s;
     pthread_t t1, t2, t3;
 
-    FILE *fp = fopen("freq.txt", "w");
+    signal(SIGINT, handle_sig);
+    signal(SIGTERM, handle_sig);
+
+    FILE *fp = fopen("logs/freq.txt", "w");
     if (fp) {
         fprintf(fp, "1000000\n");
         fclose(fp);
